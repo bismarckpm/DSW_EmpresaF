24ec0ccac30543ed214e6d7401f20f703db56ebf
Hguedez <57779032+Hguedez@users.noreply.github.com>
2020-12-01 18:35:32 -0400

    marcas table

--- src/app/config/menus/menu-brand.component.ts
+++ src/app/config/menus/menu-brand.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component,OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-menu-brand',
@@ -6,4 +8,53 @@ import { Component} from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 
-export class MenuBrandComponent {}
+
+export class MenuBrandComponent implements OnInit{
+  element:any;
+  dataSource:any;
+  displayedColumns: string[] = ['idMarca', 'nombreMarca', 'tipoMarca', 'capacidad','unidad','icons'];
+  constructor() { }
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngOnInit(): void {
+    this.getMarcas();
+    
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  getMarcas(){
+    
+    this.element = [
+      {idMarca: 1, nombreMarca: 'Hydrogen', tipoMarca: 'liso', capacidad: 'H',unidad: 2},
+      {idMarca: 2, nombreMarca: 'Helium', tipoMarca: 'liso', capacidad: 'He',unidad: 2},
+      {idMarca: 3, nombreMarca: 'Lithium', tipoMarca: 'verde', capacidad: 'Li',unidad: 2},
+      {idMarca: 4, nombreMarca: 'Beryllium', tipoMarca: 'liso', capacidad: 'Be',unidad: 2},
+      {idMarca: 5, nombreMarca: 'Boron', tipoMarca: 'negro', capacidad: 'B',unidad: 2},
+      {idMarca: 6, nombreMarca: 'Carbon', tipoMarca: 'blanco', capacidad: 'C',unidad: 2},
+      {idMarca: 7, nombreMarca: 'Nitrogen', tipoMarca: 'azul', capacidad: 'N',unidad: 2},
+      {idMarca: 8, nombreMarca: 'Oxygen', tipoMarca: 'naranja', capacidad: 'O',unidad: 2},
+      {idMarca: 9, nombreMarca: 'Fluorine', tipoMarca: 'amarillo', capacidad: 'F',unidad: 2},
+      {idMarca: 10, nombreMarca: 'Neon', tipoMarca: 'naranja', capacidad: 'Ne',unidad: 2},
+    ];
+    this.dataSource = new MatTableDataSource(this.element);
+
+  }
+
+  deleteMarca(idMarca){
+    console.log(idMarca)
+  }
+
+  updateMarca(idMarca){
+    console.log(idMarca)
+  }
+
+  addMarca(){
+    console.log("Add marca");
+  }
+}
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -25,6 +25,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatDialogModule} from '@angular/material/dialog';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 const MATERIAL_MODULES = [
   CommonModule,
@@ -47,7 +48,8 @@ const MATERIAL_MODULES = [
   MatProgressBarModule,
   MatSnackBarModule,
   MatDialogModule,
-  FlexLayoutModule
+  FlexLayoutModule,
+  MatPaginatorModule
 ]
 
 
