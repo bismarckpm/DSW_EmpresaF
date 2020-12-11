756787f97443837726c08cde655d111a8b27a79b
pancho28 <franciscobravo890@gmail.com>
2020-12-10 23:30:34 -0430

    creacion de update en config y dentro de update las vistas

--- backend/servicio/src/main/java/ucab/dsw/servicio/usuario/IServicioEmpleado.java
+++
@@ -1,9 +0,0 @@
-package ucab.dsw.servicio.usuario;
-
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
-
-public interface IServicioEmpleado {
-
-  public Response getSolicitudesPendientes(@PathParam("id") long id);
-}
