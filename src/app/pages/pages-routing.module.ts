5946a6cb5d41dbdbe9ed61e9ccaa46dda3e15d34
Hguedez <57779032+Hguedez@users.noreply.github.com>
2020-12-14 18:01:25 -0400

    ..

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { RespondentComponent } from 'src/app/pages/respondent/respondent.component';
-import { QuestionsComponent } from 'src/app/pages/questions/questions.component';
 import { GlobalMenuPComponent } from './global-menuP.component';
 import { ClientComponent } from './client/client.component';
-import { PollComponent } from 'src/app/pages/poll/poll.component';
-import { StudyComponent } from 'src/app/pages/study/study.component';
 import { RequestStudyComponent } from "src/app/pages/request-study/request-study.component";
 
 const routes: Routes = [
