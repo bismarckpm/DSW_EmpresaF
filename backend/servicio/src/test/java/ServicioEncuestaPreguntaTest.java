c317277d9e3b25aa0964e4c66be2817d2da91f7d
Julio <diaz.cesar.julio@gmail.com>
2020-12-15 02:59:16 -0400

    feat: Agregar y obtener preguntas en/de una encuesta (test)

---
+++ backend/servicio/src/test/java/ServicioEncuestaPreguntaTest.java
@@ -0,0 +1,57 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.Response;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import ucab.dsw.dtos.EncuestaDto;
+import ucab.dsw.dtos.PreguntaDto;
+import ucab.dsw.servicio.encuesta.ServicioEncuestaPregunta;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ServicioEncuestaPreguntaTest {
+
+    @Test
+    public void t1_addPreguntasToEncuesta() throws Exception {
+        ServicioEncuestaPregunta servicioEncuestaPregunta;
+        servicioEncuestaPregunta = new ServicioEncuestaPregunta();
+
+        EncuestaDto encuestaDto = new EncuestaDto();
+        encuestaDto.setId(2);
+
+        List<PreguntaDto> preguntas = new ArrayList<>();
+
+        PreguntaDto preguntaUno = new PreguntaDto(49);
+        PreguntaDto preguntaDos = new PreguntaDto(51);
+        PreguntaDto preguntaTres = new PreguntaDto(50);
+
+        preguntas.add(preguntaUno);
+        preguntas.add(preguntaDos);
+        preguntas.add(preguntaTres);
+
+        Response resultado = servicioEncuestaPregunta
+                .addPreguntasToEncuesta(encuestaDto.getId(), preguntas);
+
+        System.out.println(resultado.getEntity().toString());
+
+        Assert.assertEquals(200, resultado.getStatus());
+    }
+
+    @Test
+    public void t2_getPreguntas() throws Exception {
+        ServicioEncuestaPregunta servicioEncuestaPregunta;
+        servicioEncuestaPregunta = new ServicioEncuestaPregunta();
+
+        EncuestaDto encuestaDto = new EncuestaDto();
+        encuestaDto.setId(2);
+
+        Response resultado = servicioEncuestaPregunta.getPreguntas(encuestaDto.getId());
+
+        System.out.println(resultado.getEntity().toString());
+
+        Assert.assertEquals(200, resultado.getStatus());
+    }
+
+}
