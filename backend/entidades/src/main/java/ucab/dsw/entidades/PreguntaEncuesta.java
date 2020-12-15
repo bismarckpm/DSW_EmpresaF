b87568c36c57ddc950abd9075f198f00d528cbcb
Julio <diaz.cesar.julio@gmail.com>
2020-12-14 23:03:20 -0400

    feat: mapeo de respuestas + get y set

--- backend/entidades/src/main/java/ucab/dsw/entidades/PreguntaEncuesta.java
+++ backend/entidades/src/main/java/ucab/dsw/entidades/PreguntaEncuesta.java
@@ -1,52 +1,64 @@
 package ucab.dsw.entidades;
 
+import java.util.List;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "pregunta_encuesta")
 public class PreguntaEncuesta {
 
-  @Id
-  @Column( name = "codigo_preg_encu" )
-  @GeneratedValue( strategy = GenerationType.IDENTITY )
-  private long _id;
+    @Id
+    @Column(name = "codigo_preg_encu")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long _id;
 
-  @ManyToOne
-  @JoinColumn(name = "fk_pregunta")
-  private Pregunta _pregunta;
+    @ManyToOne
+    @JoinColumn(name = "fk_pregunta")
+    private Pregunta _pregunta;
 
-  @ManyToOne
-  @JoinColumn(name = "fk_encuesta")
-  private Encuesta _encuesta;
+    @ManyToOne
+    @JoinColumn(name = "fk_encuesta")
+    private Encuesta _encuesta;
 
-  public PreguntaEncuesta(long _id) {
-    this._id = _id;
-  }
+    @OneToMany(mappedBy = "_preguntaEncuesta", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private List<Respuesta> _respuestas;
 
-  public PreguntaEncuesta() {
-  }
+    public PreguntaEncuesta(long _id) {
+        this._id = _id;
+    }
 
-  public long get_id() {
-    return _id;
-  }
+    public PreguntaEncuesta() {
+    }
 
-  public void set_id(long _id) {
-    this._id = _id;
-  }
+    public long get_id() {
+        return _id;
+    }
 
-  public Pregunta get_pregunta() {
-    return _pregunta;
-  }
+    public void set_id(long _id) {
+        this._id = _id;
+    }
 
-  public void set_pregunta(Pregunta _pregunta) {
-    this._pregunta = _pregunta;
-  }
+    public Pregunta get_pregunta() {
+        return _pregunta;
+    }
 
-  public Encuesta get_encuesta() {
-    return _encuesta;
-  }
+    public void set_pregunta(Pregunta _pregunta) {
+        this._pregunta = _pregunta;
+    }
 
-  public void set_encuesta(Encuesta _encuesta) {
-    this._encuesta = _encuesta;
-  }
+    public Encuesta get_encuesta() {
+        return _encuesta;
+    }
+
+    public void set_encuesta(Encuesta _encuesta) {
+        this._encuesta = _encuesta;
+    }
+
+    public List<Respuesta> get_respuestas() {
+        return _respuestas;
+    }
+    
+    public void set_respuestas(List<Respuesta> respuestas) {
+        this._respuestas = _respuestas;
+    }
 }
